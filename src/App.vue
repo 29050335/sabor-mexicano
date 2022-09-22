3ae4d86a9f3dc8d7cea5134e64dcce01cf158f5d
29050335 <a3521110036@alumno.uttehuacan.edu.mx>
2022-09-22 10:57:31 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+ 
+  <Card url="https://images.aws.nestle.recipes/original/32bfcaf831e22d6d7ec102ceda99044d_Maggi_-_Chicken_Tacos.jpg" nombre="Tacos" descripcion="El taco es una preparación culinaria muy popular de México." costo="$55.00" />
+  <Card url="https://t1.rg.ltmcdn.com/es/posts/1/9/3/pizza_casera_31391_600_square.jpg" nombre="Pizza" descripcion="La pizza que unos cuestionan pero todos aman. Jamón, piña." costo="$70.99" />
+  <Card url="https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2020/06/papas-fritas-con-maicena-facil.jpg" nombre="Papas" descripcion="es un producto elaborado a base de papa en forma de rebanadas onduladas." costo="$65.99" />
+  <Card url="https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2020/12/como-preparar-espagueti-rojo-con-crema-1-kilo.jpg" nombre="Espaguetti" descripcion="es un tipo de pasta italiana elaborada con harina de grano duro y agua." costo="$40.00" />
+  <Card url="https://www.comedera.com/wp-content/uploads/2021/12/empanadas-colombianas1-500x500.jpg" nombre="Empanadas" descripcion="fina masa de pan, masa quebrada u hojaldre rellena con una preparación salada o dulce y cocida al horno o frita. " costo="$99.00" />
+  <Card url="https://www.tuhogar.com/content/dam/cp-sites/home-care/tu-hogar/es_co/recetas/comidas-faciles-y-rapidas/como-hacer-empanadas-colombianas/empanadas-colombianas-thumb-axion.jpg" nombre="hamburguesas" descripcion="sándwich hecho a base de carne molida o de origen vegetal." costo="$80.98" />
+
+</template>
+
+<script>
+  import Card from "./components/Card.vue"
+  export default {
+    components:{
+      Card,
+    },
+  };
+</script>
---
+++ src/components/Card.vue
@@ -0,0 +1,51 @@
+<template>
+  <Grid>
+      <img :src="url" :alt="nombre">
+      <h2>{{ nombre }}</h2> 
+      <h4>{{ descripcion }}</h4><br>
+      <h3>{{ costo }}</h3><br>
+      <a :href="url" target="_blank">Comprar</a>
+  </Grid>
+</template>
+
+<script>
+  import Grid from "./Grid.vue";
+  export default{
+  props: {
+      url: {
+          String,
+      },
+      nombre: {
+          String,
+      },
+      descripcion: {
+          String,
+      },
+      costo: {
+          String,
+      },
+  },
+  components: { Grid },
+};
+</script>
+
+<style scopet>
+
+  img{
+      width: 8  rem;
+      height: 8rem;
+      padding: 0.5rem;
+      background: snow;
+      border-radius: 30px;
+  }
+  a{
+      text-decoration: none;
+      color: rgb(255, 255, 255);
+      background: #ffe4c0cc;
+      padding: 0.3rem;
+      border-radius: 5px;
+      border: 1px solid #ffffff;
+      margin-bottom: 60px;
+  }
+  
+</style>
